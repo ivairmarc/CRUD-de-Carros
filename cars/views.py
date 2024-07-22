8f25b3990207a3c1d534b1fb7535ad56e2abdd81
ivairmarc <ivairwork@outlook.com>
2024-07-22 16:05:11 -0400

    class view django

--- cars/views.py
+++ cars/views.py
@@ -1,29 +1,35 @@
 from django.shortcuts import render, redirect
+from django.views import View
 from cars.models import Car
 from cars.forms import CarModelForm
 
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
-    
-    if search:
-       cars = Car.objects.filter(model__icontains=search).order_by('model')
+class CarsView(View):
 
-    return render(request, 'cars.html', {'cars': cars})
+   def get(self, request):
+      cars = Car.objects.all().order_by('model')
+      search = request.GET.get('search')
+      
+      if search:
+         cars = Car.objects.filter(model__icontains=search).order_by('model')
 
+      return render(request, 'cars.html', {'cars': cars})
+      
 
-def new_car_view(request):
-   if request.method == 'POST':
+class NewCarView(View):
+
+   def post(self, request):
       new_car_form =CarModelForm(request.POST, request.FILES)
       if new_car_form.is_valid():
          new_car_form.save()
          return redirect('cars_list')
-   else:
-      new_car_form = CarModelForm()
+   
+   def get(self, request):
+         new_car_form = CarModelForm()
 
-   return render(request, 'new_car.html', { 'new_car_form': new_car_form })
+         return render(request, 'new_car.html', { 'new_car_form': new_car_form })
 
 
-def update_car_view(request):
+class UpdateCarView(View):
    ...
+
