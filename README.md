3d33b977e7437ecc77c95d2c3ee13263c48f6c6a
ivairmarc <ivairwork@outlook.com>
2024-07-22 14:21:59 -0400

    tela de login

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
+#tela de login
+
+![alt text](/read_img/login.png)
+
 # tela Principal
 
 Primeiro Template(base_generic.html)
 
-![alt text](/read_img/image.png)
+![alt text](/read_img/list_old.png)
 
 Novo Template (base.html)
 
-![alt text](/read_img/image-2.png)
+![alt text](/read_img/list_new.png.png)
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
 
@@ -12,3 +13,22 @@ def register_view(request):
     else:
         user_form = UserCreationForm()
     return render(request, 'register.html', {'user_form': user_form})
+
+
+def login_view(request):
+
+    login_form = AuthenticationForm(request.POST)
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('cars_list')
+        else:
+            login_form = AuthenticationForm()
+        
+    else:
+        login_form = AuthenticationForm()
+        
+    return render(request, 'login.html', {'login_form': login_form})
