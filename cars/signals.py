5155039a492f2fa6f580dcea47a4045bfa9ab3f0
ivairmarc <ivairwork@outlook.com>
2024-07-24 16:09:13 -0400

    table Inventory

--- cars/signals.py
+++ cars/signals.py
@@ -1,11 +1,25 @@
 from django.db.models.signals import pre_save, pre_delete, post_delete, post_save
 from django.dispatch import receiver
-from cars.models import Car
+from django.db.models import Sum
+from cars.models import Car, CarInventory
 
-@receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    print('pre save', instance.brand)
 
-@receiver(pre_delete, sender=Car)
-def car_pre_delete(sender, instance, **Kwargs):
-    print('delete', instance)
+def car_inventory_update():
+    cars_count = Car.objects.all().count()
+    cars_value = Car.objects.aggregate(
+        total_value=Sum('value')
+    )['total_value']
+    CarInventory.objects.create(
+        cars_count = cars_count,
+        cars_value = cars_value
+    )
+
+
+@receiver(post_delete, sender=Car)
+def car_post_delete(sender, instance, **kwargs):
+    car_inventory_update()
+
+
+@receiver(post_save, sender=Car)
+def car_post_save(sender, instance, **Kwargs):
+    car_inventory_update()
