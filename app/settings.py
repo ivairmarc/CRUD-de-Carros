ca6e2f4fef39a5a37acc4f5fd61c29b935db6f35
ivairmarc <ivairwork@outlook.com>
2024-07-25 15:59:27 -0400

    list and create brands

--- app/settings.py
+++ app/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'carros_dados',  
         'USER': 'postgres',
-        'PASSWORD': 'comspirace',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
